1af12d92bf995ff9c411220614c5444061ddb20c
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-05-11 16:08:10 -0500

    Use Redux in React components
    - Wrapped the app on index.js inside the Provider who gives the state context to the app
    - Imported Redux configureStore on its respective component
    - Imported the reducers who provide the action
    - Checked all linters

--- src/components/Createbook.jsx
+++ src/components/Createbook.jsx
@@ -1,23 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Createbook.css';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/bookSlice';
 
-const Createbook = () => (
-  <>
-    <div className="bookForm">
-      <h3 className="formTitle">ADD NEW BOOK</h3>
-      <form className="createBook">
-        <input placeholder="Book Title" type="text" id="title" name="title" />
-        <br />
-        <br />
+const Createbook = () => {
+  const dispatch = useDispatch();
 
-        <input placeholder="Book Author" type="text" id="author" name="author" />
-        <br />
-        <br />
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
 
-        <input type="submit" value="Add Book" />
-      </form>
-    </div>
-  </>
-);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newBook = {
+      item_id: Math.random().toString(36).substr(2, 9),
+      title,
+      author,
+      category: 'Fiction', // or whatever category you want to assign by default
+    };
+
+    dispatch(addBook(newBook));
+    setTitle('');
+    setAuthor('');
+  };
+
+  return (
+    <>
+      <div className="bookForm">
+        <h3 className="formTitle">ADD NEW BOOK</h3>
+        <form className="createBook" onSubmit={handleSubmit}>
+          <input
+            placeholder="Book Title"
+            type="text"
+            id="title"
+            name="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+          <br />
+          <br />
+
+          <input
+            placeholder="Book Author"
+            type="text"
+            id="author"
+            name="author"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            required
+          />
+          <br />
+          <br />
+
+          <input type="submit" value="Add Book" />
+        </form>
+      </div>
+    </>
+  );
+};
 
 export default Createbook;
